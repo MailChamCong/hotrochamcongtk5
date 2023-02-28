e88fba7e86dece4c178929e1b6352345b92fa46a
tiendung1112 <traitre37@gmail.com>
2023-02-28 10:29:40 +0700

    commit

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function FSendMailOTPTimviec365(title,content,receiver){
         let transporter = nodemailer.createTransport({
             service:'gmail',
             auth:{
-            user:'timviec365.vn.3@gmail.com',
-            pass:'bpeowdqnjajcdssf'
+            user:'work247.vn.3@gmail.com',
+            pass:'phiyvrobuzhgeqjp'
             }
         });
         const mail_config = {
-            from:'timviec365.vn.3@gmail.com',
+            from:'work247.vn.3@gmail.com',
             to:receiver,
             subject:title,
             html: `${content}`
